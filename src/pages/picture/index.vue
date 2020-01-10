57a403cb673eb1353f1d0e287c55c0558ce78d64
Sfl <942368681@qq.com>
2020-01-10 15:36:10 +0800

    测试svg

---
+++ src/pages/picture/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <image :src="src" mode="aspectFit"></image>
+    </div>
+</template>
+
+<script>
+import svg from '../../../static/images/pic1.svg';
+// import png from '../../../static/images/user.png';
+
+export default {
+    data() {
+        return {
+            src: svg
+        };
+    },
+
+    components: {},
+    created() {
+        
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style scoped>
+.container {
+
+}
+
+</style>
